ee889b16ac3d2cbc3ff29d77e61a00a508bd3bbd
Salvador Gómez Jaime <salvador_gj@hotmail.com>
2017-09-15 16:29:23 +0200

    c¡omit b

--- app/Repositories/Shows.php
+++ app/Repositories/Shows.php
@@ -13,11 +13,7 @@ Class Shows
     	$this->client = new Client(['base_uri' => 'http://api.tvmaze.com/search/','timeout'=>2.0]);
   	}
 
-	public function all(){
-        $response = $this->client->request('GET','shows?q=game+of+thrones');
-        return $response;
-	}
-	
+
 	public function findShow($text_string){
 		$response = $this->client->request('GET','shows?q='.$text_string);
 		return $response;
